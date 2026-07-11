48e477f91b93f62c488876decffc22000e10d219
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2026-07-11 10:46:11 +0700

    all: rewrite neovim config!!!
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

---
+++ plugin/lsp.lua
@@ -0,0 +1,7 @@
+vim.pack.add({ "https://github.com/neovim/nvim-lspconfig" })
+
+vim.lsp.enable("dexter")
+vim.lsp.enable("zls")
+vim.lsp.enable("ruff")
+vim.lsp.enable("rust_analyzer")
+vim.lsp.enable("gopls")
